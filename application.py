13657d41a960845fc52083e8187c0a820d327e40
keith72632 <keithhorton30@outlook.com>
2021-10-27 12:41:28 -0500

    aws test

--- application.py
+++ application.py
@@ -19,6 +19,3 @@ def predict():
     output = round(prediction[0], 2)
     return render_template('index.html', prediction_text='CO2    Emission of the vehicle is :{}'.format(output))
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
