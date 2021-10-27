ab68f8f0258efd9a6731dc315682b38c523c51f8
keith72632 <keithhorton30@outlook.com>
2021-10-27 14:42:02 -0500

    here we go

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-# import numpy as np
-from flask import Flask#, render_template, request
-# import pickle
+import numpy as np
+from flask import Flask, render_template, request
+import pickle
 
 application = Flask(__name__)
 # model = pickle.load(open('model.pkl', 'rb'))
@@ -8,16 +8,14 @@ application = Flask(__name__)
 #default page of our web-app
 @application.route('/')
 def home():
-    # return render_template('index.html')
-    return 'Hey'
+    return render_template('index.html')
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     #for rendering results on HTML GUI
-#     # int_features = [float(x) for x in request.form.values()]
-#     # final_features = [np.array(int_features)]
-#     # prediction = model.predict(final_features)
-#     # output = round(prediction[0], 2)
-#     # return render_template('index.html', prediction_text='CO2    Emission of the vehicle is :{}'.format(output))
-#     return 'predict'
+@application.route('/predict', methods=['POST'])
+def predict():
+    # for rendering results on HTML GUI
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text='CO2    Emission of the vehicle is :{}'.format(output))
 
